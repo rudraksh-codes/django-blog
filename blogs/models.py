b77f25bcb99482ea4c4b186793398c97377a6e2c
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-12 22:47:24 +0530

    feat: add blogs model

---
+++ blogs/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(
+        max_length=50, 
+        unique=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    ) 
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta: 
+       verbose_name_plural = "categories"
+
+class Blog(models.Model):
+
+    class Statuses(models.TextChoices):
+        COMPLETED = 'completed', 'Completed'
+        IN_PROGRESS = 'in_progress', 'In_Progress'
+        PENDING = 'pending', 'Pending'
+
+
+    title = models.CharField(
+        max_length=50,
+    )
+
+    slug = models.SlugField( 
+        max_length=100,
+    )
+
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE, 
+        related_name="blogs", 
+    )
+
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name = "blogs"
+    )
+
+    featured_image = models.ImageField(
+        upload_to= "uploads/%Y/%m/%d",
+    )
+
+    featured_document_file = models.FileField(
+        upload_to="uploads/%Y/%m/%d", 
+    )
+
+    short_description = models.TextField(
+        max_length=500
+    )
+
+    blog_body = models.TextField(max_length=2000)
+
+    status = models.CharField(
+        max_length=20, 
+        choices = Statuses, 
+        default= Statuses.PENDING
+
+    )
+
+    is_featured = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
