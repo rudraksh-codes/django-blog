4ff9c5fd7444d83b2c9ab4882008cab18da2b4ca
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-14 00:53:45 +0530

    add list-display and search-fields in django admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Category, Blog
 
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug" : ("title",)
+    } 
+
+    list_display = ('status', 'title', 'category', 'author', 'is_featured')
+    search_fields = ('id', 'title', 'category__category_name', 'status', 'author__username')
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
+
+
+
