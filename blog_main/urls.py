e875cb7d27fa3eb3b723e721566e52e360536032
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-23 12:43:58 +0530

    feat: add search feature

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
     path('category/', include('blogs.urls')), 
-    path('<slug:slug>/', BlogsViews.blogs, name = "blogs")
+    path('<slug:slug>/', BlogsViews.blogs, name = "blogs"), 
+    #search
+    path('blogs/search', BlogsViews.search, name = "search"),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
