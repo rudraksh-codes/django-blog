7fc96be5a5e6a1ba28189f35c029ed4fb70f8956
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-25 12:25:19 +0530

    feat: add dashboards app and boilerplate

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('register/', views.register, name = 'register'), 
     path('login/', views.login, name = "login"), 
     path('logout/', views.logout, name = "logout"),  
+    path('dashboard/', include('dashboards.urls'))
 
 ] 
 
