f4384706bada2eed21a0a81eb24a0845e79639d2
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-26 13:06:42 +0530

    feat: add categories page on dashboard

--- blog_main/views.py
+++ blog_main/views.py
@@ -52,7 +52,7 @@ def login(request):
             if user is not None : 
                 auth.login(request, user)
 
-            return redirect('home')
+            return redirect('dashboard')
         else :
             print(form.errors)
     else:
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required(login_url="login")
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    category_count = Category.objects.all().count()
+    post_count = Blog.objects.all().count()
+
+    context = dict(category_count = category_count, post_count = post_count)
+
+    return render(request, "dashboard/dashboard.html", context) 
+
+def categories(request):
+    return render(request, "dashboard/categories.html")
+
+def add_category(request):
+    return render(request, "dashboard/add_category.html")
