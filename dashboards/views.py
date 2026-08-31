4d5484009b7daa7d898c070216e8e9012c9f53c7
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-31 11:25:26 +0530

    feat: category CRUD functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm
@@ -31,3 +31,24 @@ def add_category(request):
         "form" : form
     }
     return render(request, "dashboard/add_category.html", context)
+
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk = pk)
+    if request.method == "POST" : 
+        form  = CategoryForm(request.POST, instance=category) #new_value, #existing_value
+        if form.is_valid():
+            form.save()
+            return redirect("categories")
+    form = CategoryForm(instance=category)
+    context = {
+        "form" : form,
+        "category" : category, 
+    }
+    return render(request, "dashboard/edit_category.html", context)
+
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk = pk)
+    category.delete()
+    return redirect("categories")
