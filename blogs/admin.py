f8e7e62e7bb6f3737643ada7d4a096c2d19e11c7
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-09-12 15:54:51 +0530

    feat: add Comments model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Blog
+from .models import Category, Blog, Comment
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {
@@ -14,5 +14,6 @@ class BlogAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
 
 
--- blogs/models.py
+++ blogs/models.py
@@ -80,3 +80,24 @@ class Blog(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+    )
+    blog = models.ForeignKey(
+        Blog, 
+        on_delete=models.CASCADE
+    )
+    comment = models.TextField(
+        max_length=500, 
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.comment
